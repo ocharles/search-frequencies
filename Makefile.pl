be1fd27321bb670cf84d7ae3267d15d67b4aed24
Oliver Charles <oliver.g.charles@googlemail.com>
2009-10-15 13:50:09 +0100

    Some missing dependencies

--- Makefile.pl
+++ Makefile.pl
@@ -7,6 +7,9 @@ requires 'DBI';
 requires 'DateTime::Format::SQLite';
 requires 'JSON::Any';
 requires 'REST::Google';
+requires 'XML::Simple';
+requires 'Data::Entropy::Algorithms';
+requires 'Module::Pluggable';
 
 auto_install;
 WriteAll;
