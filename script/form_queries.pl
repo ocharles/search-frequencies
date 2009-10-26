2c332f34333126d2f3043f29d113dacdd0a54f6c
Oliver Charles <oliver.g.charles@googlemail.com>
2009-10-26 13:23:33 +0000

    Bring back in single word support

--- script/form_queries.pl
+++ script/form_queries.pl
@@ -2,8 +2,9 @@ use strict;
 use warnings;
 use Data::Entropy::Algorithms qw(shuffle);
 
-my @possible;
+my (@idiomatic, @words);
 
+# Parse out all the idiomatic expressions that look interesting
 open(my $fh, '<', 'idioms.c7');
 while(my $line = <$fh>) {
     chomp $line;
@@ -12,13 +13,33 @@ while(my $line = <$fh>) {
     $interesting =~ s/_\w+\b//g;
     next unless $interesting =~ /^[a-z\s]+$/;
     $interesting =~ s/\s+$//g;
-    push @possible, $interesting;
+    push @idiomatic, $interesting;
+}
+close($fh);
+
+# Parse the top 1000 single words
+open($fh, '<', '1_2_all_freq.txt');
+my $i = 0;
+while ($i < 1000) {
+    my $line = <$fh>;
+    chomp $line;
+    $line =~ s/^\s+(.*)/$1/;
+    my ($word, $type, $freq) = split /\s+/, $line;
+    next unless $word =~ /^[A-Za-z]+$/;
+    push @words, $word;
+    $i++;
 }
 close($fh);
 
 open ($fh, '>', 'queries.txt');
-my @shuffled = shuffle(@possible);
+# Take 2000 idiomatic expressions
+my @shuffled = shuffle(@idiomatic);
 for my $i (1..2000) {
     print $fh $shuffled[$i] . "\n";
+}
+
+# Take all the single words
+for my $word (@words) {
+    print $fh "$word\n";
 }
 close($fh);
